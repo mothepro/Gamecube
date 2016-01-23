718f46d1f54cb58bfeef48af1e3153a2f14acc5f
Maurice Prosper <maurice.prosper@ttu.edu>
2016-01-23 17:17:00 -0600

    Use grunt to watch & build js file don't uglify...

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            example: {
+                files: [{
+                    src: 'example/assets/script.js',
+                    dest: 'example/bundle.js'
+                }],
+                options: {
+                    transform: ['brfs']
+                }
+            }
+        },
+        uglify: {
+            example: {
+                src: 'example/bundle.js',
+                dest: 'example/bundle.min.js'
+            }
+        },
+        watch: {
+            example: {
+                files: ['*.js', 'example/assets/**.js'],
+                tasks: ['build']
+            }
+        },
+        clean: ['example/bundle.js', 'example/bundle.min.js']
+    });
+
+    // Load the npm tasks
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Tasks task(s).
+    grunt.registerTask('build', ['clean', 'browserify', 'clean:0']);
+    grunt.registerTask('default', ['watch']);
+};
